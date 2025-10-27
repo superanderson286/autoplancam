5f9837de23c99b44d31fd3b4c1ffa36381f0c212
superanderson286 <superanderson286@gmail.com>
2025-10-26 23:42:00 -0400

    minor bugs fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
-import nextTypescript from "eslint-config-next/typescript";
+import nextTypescript from "eslint-config-next/typescript.js";
 
 const eslintConfig = [
   ...nextCoreWebVitals,
